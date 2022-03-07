545cf8affc945c96530a96ca3740cafa96effc78
IngoStramm <ingostramm@gmail.com>
2022-03-07 15:23:08 -0300

    fix jquery mask conflict

--- scripts.php
+++ scripts.php
@@ -11,6 +11,13 @@ function ltk_frontend_scripts()
         wp_enqueue_script('ltk-livereload', 'http://localhost:35729/livereload.js?snipver=1', array(), null, true);
     endif;
 
+    // Remove o include 'jquery-mask' adicionado pelo plugin "Extra Checkout Fields for Brazil"
+    wp_deregister_script('jquery-mask');
+
+    // Adiciona um JS vazio, com o nome de 'jquery-mask', para o plugin "Extra Checkout Fields for Brazil" continuar chamando os scripts que dependem dele
+    wp_register_script('jquery-mask',LTK_URL . 'assets/js/jquery-mask.js', array(), false, 'all');
+
+    // Máscaras em JS para os campos (sem jQuery)
     wp_enqueue_script('vanilla-masker', LTK_URL . 'assets/js/vanilla-masker.min.js', array(), false, 'all');
 
     wp_register_script('ltk-script', LTK_URL . 'assets/js/ltk' . $min . '.js', array('jquery', 'vanilla-masker'), '1.0.0', true);
